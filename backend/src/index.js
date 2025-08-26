ce47800434d445678f78ef8be0094f58abbf2e07
ShouryaDimri <shouryadimricareer@gmail.com>
2025-08-26 23:04:07 +0530

    adding Models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+import { connectDB } from './lib/db.js';  
+
 import userRoutes from './routes/userRoutes.js';
 import adminRoutes from './routes/adminRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import songRoutes from './routes/songRoutes.js';
 import albumRoutes from './routes/albumRoutes.js';
+import statRoutes from './routes/statRoutes.js';
+
+
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 
 app.use("/api/users", userRoutes);
@@ -21,4 +27,5 @@ app.use("/api/statistics", statRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port:', PORT);
+  connectDB(process.env.MONGODB_URI);
 });
---
+++ backend/src/models/messageModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+    senderId : {
+        type : String,
+        required : true
+    },
+    receiverId : {
+        type : String,
+        required : true
+    },
+    message : {
+        type : String,
+        required : true
+    }
+}, { timestamps : true });
+
+export const Message = mongoose.model("Message", messageSchema); 
