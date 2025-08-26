4971f5e9f6401c3f9df7fbf04da0f7491fbbbbe9
ShouryaDimri <shouryadimricareer@gmail.com>
2025-08-26 20:24:09 +0530

    minor-edit

--- backend/src/routes/songRoutes.js
+++ backend/src/routes/songRoutes.js
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.send("Song route is working");
+  res.send("Song route is working correctly");
 });
 
 export default router;
